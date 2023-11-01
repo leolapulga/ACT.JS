fad71c0fe96cbbda27fa55d845852187082c99ab
leolapulga <149593339+leolapulga@users.noreply.github.com>
2023-11-01 19:08:27 +0530

    Add files via upload

---
+++ flow-typed/npm/temp-dir_2.x.x.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) Lakshit Khatkar 
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict
+ * @format
+ * @oncall react_native
+ */
+
+declare module 'temp-dir' {
+  declare module.exports: string;
+}
